a173cdec74ca5796cd888e0373710ac4ad5012fb
TANUJ SHARMA <68097949+TANUJSH10101@users.noreply.github.com>
2023-09-24 13:59:47 +0530

    data type + casting

---
+++ BasicProgramming/005DataType.py
@@ -0,0 +1,68 @@
+# a is a variable containing None
+a = None
+
+# assgning "Tanuj" to variable a
+a = "Tanuj"
+# print type of variable a
+print(type(a))
+# print valur of variable a
+print(a)
+
+# assign "Rahul" to variable a
+a = "Rahul"
+#print type of variable a
+print(type(a))
+#print valur of varible a
+print(a)
+
+# assign 1 to variable a
+a = 1
+#print type of varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+# assign a+1 to varible a
+a = a+1
+#print type of varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+# assign a+9 to varible a
+a = a+9
+#print type of varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+# assign 2.0 to varible a
+a = 2.0
+#print type of varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+
+
+# assign "tanuj" to varible a
+a = "tanuj"
+#print type to varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+# assign 1+a to varible a 
+a = a + str(1)
+#print type to varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+
+# assign 1+a to varible a 
+a = str(1) + a
+#print type to varible a
+print(type(a))
+#print valur of varible a
+print(a)
---
+++ BasicProgramming/006Casting.py
@@ -0,0 +1,21 @@
+# assign "tanuj" to varible a
+a = "tanuj"
+#print type to varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+# assign 1+a to varible a 
+a = a + str(1)
+#print type to varible a
+print(type(a))
+#print valur of varible a
+print(a)
+
+
+# assign 1+a to varible a 
+a = str(1) + a
+#print type to varible a
+print(type(a))
+#print valur of varible a
+print(a)
